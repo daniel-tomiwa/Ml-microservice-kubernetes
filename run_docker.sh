2dd69f44d1dbe80c5e9ec76e8c3019fbd3b68880
daniel-tomiwa <danielgbenga814@gmail.com>
2022-07-27 23:58:38 +0000

    changed the runing scripts and docker file content

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=ml_app .
 
 # Step 2: 
 # List docker images
+docker images ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 ml_app
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=danietomiwa/ml_app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml_app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
