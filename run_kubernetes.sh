8b776601bcf2827c7d66a754404b771786418c23
EC2 Default User <ec2-user@ip-172-31-91-56.ec2.internal>
2022-07-28 06:52:22 +0000

    run kubernetes script updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=danietomiwa/ml_app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-microservice --image=danietomiwa/mlapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/ml-microservice-659d8c998c-b4pq5 --address 0.0.0.0 8000:80
